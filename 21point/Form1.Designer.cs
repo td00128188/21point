4d8005d582cb65be838fc6a3c1eb55ea1a76a1d6
許福陞 <zxcv81602@gmail.com>
2020-11-12 18:40:07 +0800

    加入專案檔案。

---
+++ 21point/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _21point
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.C1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.c2 = new System.Windows.Forms.PictureBox();
+            this.c3 = new System.Windows.Forms.PictureBox();
+            this.c4 = new System.Windows.Forms.PictureBox();
+            this.c5 = new System.Windows.Forms.PictureBox();
+            this.deck = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p1
+            // 
+            this.p1.Location = new System.Drawing.Point(54, 248);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(105, 150);
+            this.p1.TabIndex = 0;
+            this.p1.TabStop = false;
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(203, 248);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(105, 150);
+            this.C1.TabIndex = 1;
+            this.C1.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.Location = new System.Drawing.Point(54, 215);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(105, 150);
+            this.p2.TabIndex = 2;
+            this.p2.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.Location = new System.Drawing.Point(54, 181);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(105, 150);
+            this.p3.TabIndex = 3;
+            this.p3.TabStop = false;
+            // 
+            // p4
+            // 
+            this.p4.Location = new System.Drawing.Point(54, 146);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(105, 150);
+            this.p4.TabIndex = 4;
+            this.p4.TabStop = false;
+            // 
+            // p5
+            // 
+            this.p5.Location = new System.Drawing.Point(54, 105);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(105, 150);
+            this.p5.TabIndex = 5;
+            this.p5.TabStop = false;
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(203, 215);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(105, 150);
+            this.c2.TabIndex = 6;
+            this.c2.TabStop = false;
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(203, 181);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(105, 150);
+            this.c3.TabIndex = 7;
+            this.c3.TabStop = false;
+            // 
+            // c4
+            // 
+            this.c4.Location = new System.Drawing.Point(203, 146);
+            this.c4.Name = "c4";
+            this.c4.Size = new System.Drawing.Size(105, 150);
+            this.c4.TabIndex = 8;
+            this.c4.TabStop = false;
+            // 
+            // c5
+            // 
+            this.c5.Location = new System.Drawing.Point(203, 105);
+            this.c5.Name = "c5";
+            this.c5.Size = new System.Drawing.Size(105, 150);
+            this.c5.TabIndex = 9;
+            this.c5.TabStop = false;
+            // 
+            // deck
+            // 
+            this.deck.Location = new System.Drawing.Point(366, 105);
+            this.deck.Name = "deck";
+            this.deck.Size = new System.Drawing.Size(105, 150);
+            this.deck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.deck.TabIndex = 10;
+            this.deck.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.pb1.Location = new System.Drawing.Point(366, 277);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(94, 41);
+            this.pb1.TabIndex = 11;
+            this.pb1.Text = "要牌";
+            this.pb1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.button1.Location = new System.Drawing.Point(366, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "PASS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 40);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "1對1 21點 模擬器";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 448);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.deck);
+            this.Controls.Add(this.c5);
+            this.Controls.Add(this.c4);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.p1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deck)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox C1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox c2;
+        private System.Windows.Forms.PictureBox c3;
+        private System.Windows.Forms.PictureBox c4;
+        private System.Windows.Forms.PictureBox c5;
+        private System.Windows.Forms.PictureBox deck;
+        private System.Windows.Forms.Button pb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 21point/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21point
+{
+    public partial class Form1 : Form
+    {
+        List <Image> = new  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
